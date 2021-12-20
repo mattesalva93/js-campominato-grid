4f26d235765ff598ca7c2d4169a1c9dccb52f0e4
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-20 16:58:36 +0100

    click not working

--- myscript.js
+++ myscript.js
@@ -8,48 +8,30 @@
 
 */
 
-//creo una funzione che crei una casella basata sul numero che le inserisco
-function creaCasella (livelloDifficolta){
-    let campoMinatoBox = "";
-
-    for (i=0; i<livelloDifficolta; i++){
-        if (livelloDifficolta == 100){
-        campoMinatoBox += `
-        <div id="minesweeper-box" class="col- ms_minesweeper-box-100 d-flex justify-content-center align-items-center">
-            ${i + 1}          
-        </div>
-        `;
-    }else if (livelloDifficolta == 81){
-        campoMinatoBox += `
-        <div id="minesweeper-box" class="col- ms_minesweeper-box-81 d-flex justify-content-center align-items-center">
-            ${i + 1}          
-        </div>
-        `;
-    }else if (livelloDifficolta == 49){
-        campoMinatoBox += `
-        <div id="minesweeper-box" class="col- ms_minesweeper-box-49 d-flex justify-content-center align-items-center">
-            ${i + 1}          
-        </div>
-        `;
-    }
-    }
-        
-    const creaBox = document.getElementById("minesweeper-grid");
-    creaBox.innerHTML = campoMinatoBox;
-};
-
 //faccio scegliere all'utente il livello di difficoltà alla quale vuole giocare
-
 let pulsanteScelta = document.getElementById("levelchoice");
 
 pulsanteScelta.addEventListener("click", function(){
     let difficoltaScelta = document.getElementById("difficult");
     let difficolta = difficoltaScelta.value;
-    console.log(difficolta);
 
-    creaCasella(difficolta);
+    let campoMinatoBox = "";
 
-    
+    for (i=0; i<difficolta; i++){
+        campoMinatoBox += `
+        <div id="minesweeper-box" class="col- ms_minesweeper-box-${difficolta} d-flex justify-content-center align-items-center">
+            ${i + 1}          
+        </div>
+        `;
+    }
+        
+    const creaBox = document.getElementById("minesweeper-grid");
+    creaBox.innerHTML = campoMinatoBox;
+
+    let casellaScelta = document.getElementById("minesweeper-box");
+    casellaScelta.addEventListener("click", function(){
+        casellaScelta.classList.add('ms_select-box');
+    })
 });
 
 
