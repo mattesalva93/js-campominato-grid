0af93286b77c29391474a9a0809e373e033bed11
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-20 16:33:54 +0100

    no click event

--- myscript.js
+++ myscript.js
@@ -13,17 +13,44 @@ function creaCasella (livelloDifficolta){
     let campoMinatoBox = "";
 
     for (i=0; i<livelloDifficolta; i++){
+        if (livelloDifficolta == 100){
         campoMinatoBox += `
-        <div class="col- text-center ms_minesweeper-box">
+        <div id="minesweeper-box" class="col- ms_minesweeper-box-100 d-flex justify-content-center align-items-center">
             ${i + 1}          
         </div>
         `;
+    }else if (livelloDifficolta == 81){
+        campoMinatoBox += `
+        <div id="minesweeper-box" class="col- ms_minesweeper-box-81 d-flex justify-content-center align-items-center">
+            ${i + 1}          
+        </div>
+        `;
+    }else if (livelloDifficolta == 49){
+        campoMinatoBox += `
+        <div id="minesweeper-box" class="col- ms_minesweeper-box-49 d-flex justify-content-center align-items-center">
+            ${i + 1}          
+        </div>
+        `;
+    }
     }
         
     const creaBox = document.getElementById("minesweeper-grid");
     creaBox.innerHTML = campoMinatoBox;
 };
 
+//faccio scegliere all'utente il livello di difficoltà alla quale vuole giocare
+
+let pulsanteScelta = document.getElementById("levelchoice");
+
+pulsanteScelta.addEventListener("click", function(){
+    let difficoltaScelta = document.getElementById("difficult");
+    let difficolta = difficoltaScelta.value;
+    console.log(difficolta);
+
+    creaCasella(difficolta);
+
+    
+});
 
 
 
