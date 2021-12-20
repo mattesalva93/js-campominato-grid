d8f01c67aca302fd3ae1b36620439cb7a82a6928
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-20 18:09:30 +0100

    all done

--- myscript.js
+++ myscript.js
@@ -1,38 +1,46 @@
-/*
-    faccio una select con 3 opzioni
-    event listener click per generare la griglia
-    genero una casella a ripetizione
-    dentro i numeri relativi
-    easy 10x10 medium 9x9 hard 7x7
-    al click cambia colore delle caselle
 
-*/
+// creo una funzione generica per far si che selezionando i box si colorino 
+function selettoreBox(elenco){
+    for (let i = 0; i < elenco.length; i++) {
+
+        elenco[i].addEventListener("click",function(){
+            this.classList.add("ms_select-box");
+    
+        });
+    }   
+
+};
 
 //faccio scegliere all'utente il livello di difficoltà alla quale vuole giocare
 let pulsanteScelta = document.getElementById("levelchoice");
 
+//al click compare la griglia
 pulsanteScelta.addEventListener("click", function(){
     let difficoltaScelta = document.getElementById("difficult");
     let difficolta = difficoltaScelta.value;
 
+    //inserisco una griglia nell'html basandomi sul value della difficoltà cliccata
     let campoMinatoBox = "";
 
     for (i=0; i<difficolta; i++){
         campoMinatoBox += `
-        <div id="minesweeper-box" class="col- ms_minesweeper-box-${difficolta} d-flex justify-content-center align-items-center">
+        <div id="box-${i + 1}" class="col- ms_selector ms_minesweeper-box-${difficolta} d-flex justify-content-center align-items-center">
             ${i + 1}          
         </div>
         `;
-    }
         
+    }
+    
+    //dichiaro dove voglio che questi box vengano creati
     const creaBox = document.getElementById("minesweeper-grid");
     creaBox.innerHTML = campoMinatoBox;
 
-    let casellaScelta = document.getElementById("minesweeper-box");
-    casellaScelta.addEventListener("click", function(){
-        casellaScelta.classList.add('ms_select-box');
-    })
+    //creo un array che contenga tutti miei box selezionandoli per la classe
+    let elencoBox = document.getElementsByClassName("ms_selector");
+    console.log(elencoBox); 
+
+    //faccio partire la funzione per colorarle al click
+    selettoreBox(elencoBox);
+
 });
-
-
 
